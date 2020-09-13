06e56e226955c47e85ce0ae30517fae5d291de69
Guilherme <guilhermevictor20@gmail.com>
2020-09-13 18:56:08 -0300

    separated responsibilities between classes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,27 +1,83 @@
-class Data {
+class FetchedData {
     constructor(){
-        this.liElement = document.querySelectorAll('li')
-        this.selectElement = document.querySelector('#selectCountry')
-        this.textBox = document.querySelector('.text-box')
+        this.url = 'https://api.covid19api.com/summary'
     }
 
-    async run(){
-        await this.fillSelect()
-        await this.renderGlobalData()
-        this.selectAction()
-        this.getDate()
-    }
-
-    async getData(){
+    async getAllData(){
         const response = await fetch('https://api.covid19api.com/summary')
         const data = await response.json()
 
         return data
     }
 
+    async getGlobalData(){
+        const allData = await this.getAllData()
+        const globalData = allData.Global
+
+        return globalData
+    }
+
+    async getCountriesData(){
+        const allData = await this.getAllData()
+        const countries = allData.Countries
+
+        return countries
+    }
+
+    async getFullDate(){
+        const data = await this.getAllData()
+        const date = data.Date
+
+        return date
+    }
+}
+
+const Data = new FetchedData()
+
+class RenderFormatedDate{
+    constructor(){
+        this.textBox = document.querySelector('.text-box')
+        this.date = Data.getFullDate()
+    }
+
+    async formatDate(){
+        const date = await this.date
+        
+        const dateArr = date.split('T')[0].split('-')
+        const year = dateArr[0]
+        const month = dateArr[1]
+        const day = dateArr[2]
+
+        const formatedDate = `${day}/${month}/${year}`
+
+        this.renderDate(formatedDate)
+    }
+
+    renderDate(date){
+        this.textBox.innerHTML += `<p>Última atualização: <span>${date}</span></p>`
+    }
+}
+
+const Date = new RenderFormatedDate()
+Date.formatDate()
+
+class RenderData {
+    constructor(){
+        this.liElement = document.querySelectorAll('li')
+        this.selectElement = document.querySelector('#selectCountry')
+
+        this.globalData = Data.getGlobalData()
+        this.countriesData = Data.getCountriesData()
+    }
+
+    async run(){
+        await this.fillSelect()
+        await this.renderGlobalData()
+        this.selectAction()
+    }
+    
     async fillSelect(){
-        const data = await this.getData()
-        const countries = data.Countries
+        const countries = await this.countriesData
         
         countries.map(country => {
             const {CountryCode, Country} = country
@@ -41,22 +97,20 @@ class Data {
         })
     }
 
+    async renderGlobalData(){
+        const globalData = await this.globalData
+        const {TotalDeaths,TotalConfirmed,TotalRecovered} = globalData
+
+        this.renderValues(TotalDeaths,TotalConfirmed,TotalRecovered)
+    }
+
     async renderCountryData(value){
-        const data = await this.getData()
-        const countries = data.Countries
+        const countries = await this.countriesData
 
         countries.map(country => {
             const { CountryCode, TotalConfirmed, TotalDeaths, TotalRecovered } = country
             if(CountryCode === value) this.renderValues(TotalDeaths, TotalConfirmed, TotalRecovered)
         })
-    }
-
-    async renderGlobalData(){
-        const data = await this.getData()
-        const globalData = data.Global
-        const {TotalDeaths,TotalConfirmed,TotalRecovered} = globalData
-
-        this.renderValues(TotalDeaths,TotalConfirmed,TotalRecovered)
     }
 
     renderValues(totalDeaths,totalConfirmed,totalRecovered){
@@ -72,30 +126,7 @@ class Data {
     numberWithCommas(number){
         return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
-
-    renderDate(formatedDate){
-        this.textBox.innerHTML += `<p>Última atualização: <span>${formatedDate}</span></p>`
-    }
-
-    formatDate(date){   
-        const dateArr = date.split('T')[0].split('-')
-        const year = dateArr[0]
-        const month = dateArr[1]
-        const day = dateArr[2]
-
-        const formatedDate = `${day}/${month}/${year}`
-
-        return formatedDate
-    }
-
-    async getDate(){
-        const data = await this.getData()
-        const { Date } = data
-
-        const formatedDate = this.formatDate(Date)
-        this.renderDate(formatedDate)
-    }
 }
 
-const data = new Data()
-data.run()
+const render = new RenderData()
+render.run()
